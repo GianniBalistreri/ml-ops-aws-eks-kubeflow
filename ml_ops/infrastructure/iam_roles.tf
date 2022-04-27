0f828254c5a44fff89ba4afecafc40b4e410cea4
giannibalistreri <gbalistreri762@gmail.com>
2022-04-27 20:47:18 +0200

    Add OIDC support for Github Actions

--- ml_ops/infrastructure/iam_roles.tf
+++ ml_ops/infrastructure/iam_roles.tf
@@ -65,3 +65,21 @@ data "aws_iam_policy_document" "s3" {
     ]
   }
 }
+
+
+###################################
+# Github Action Identity Provider #
+###################################
+
+resource "aws_iam_openid_connect_provider" "githubOidc" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+}
+
+resource "aws_iam_role" "github_role" {
+  name               = "GithubActionsRole"
+  assume_role_policy = data.aws_iam_policy_document.github_allow.json
+}
