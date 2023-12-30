850ab130a201985877857ed0631a889fe8ad807b
giannibalistreri <g.balistreri@shopware.com>
2023-12-30 04:22:18 +0100

    Fixed bug in function "save_file_to_s3"

--- kubeflow_templates/kubeflow_pipelines/task_pool/v1/evolutionary_algorithm/task_container/src/aws.py
+++ kubeflow_templates/kubeflow_pipelines/task_pool/v1/evolutionary_algorithm/task_container/src/aws.py
@@ -84,19 +84,22 @@ def save_file_to_s3(file_path: str, obj) -> None:
     _bucket_name: str = _complete_file_path.split('/')[0]
     _file_path: str = _complete_file_path.replace(_bucket_name, '')
     _file_type: str = _complete_file_path.split('.')[-1]
-    _s3_resource: boto3 = boto3.resource('s3')
-    _s3_obj: _s3_resource.Object = _s3_resource.Object(_bucket_name, _file_path)
+    _s3_client: boto3.client = boto3.client('s3')
     if _file_type == 'json':
-        _s3_obj.put(Body=json.dumps(obj=obj))
+        _s3_client.put_object(Body=json.dumps(obj=obj), Bucket=_bucket_name, Key=_file_path)
     elif _file_type in ['p', 'pkl', 'pickle']:
-        _s3_obj.put(Body=pickle.dumps(obj=obj, protocol=pickle.HIGHEST_PROTOCOL))
+        _s3_client.put_object(Body=pickle.dumps(obj=obj, protocol=pickle.HIGHEST_PROTOCOL), Bucket=_bucket_name, Key=_file_path)
     elif _file_type == 'txt':
         _buffer: io.StringIO = io.StringIO()
         _buffer.write(obj)
-        _s3_obj.put_object(Body=_buffer.getvalue(), Bucket=_buffer, Key=_file_path)
+        _s3_client.put_object(Body=_buffer.getvalue(), Bucket=_bucket_name, Key=_file_path)
     elif _file_type == 'html':
         _buffer: io.StringIO = io.StringIO()
         _buffer.write(obj.to_html())
-        _s3_obj.put_object(Body=_buffer.getvalue(), Bucket=_buffer, Key=_file_path)
+        _s3_client.put_object(Body=_buffer.getvalue(), Bucket=_bucket_name, Key=_file_path)
+    elif _file_type in ['png', 'jpg', 'jpeg']:
+        _buffer: io.BytesIO = io.BytesIO()
+        _buffer.write(obj)
+        _s3_client.put_object(Body=_buffer.getvalue(), Bucket=_bucket_name, Key=_file_path)
     else:
         raise AWSS3Exception(f'Saving file type ({_file_type}) not supported')
