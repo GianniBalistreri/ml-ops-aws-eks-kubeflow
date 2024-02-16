589c8381e21fe99fe64b687d1234c58b38fa0561
giannibalistreri <g.balistreri@shopware.com>
2024-02-16 21:50:44 +0100

    Deletion

--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/analytical_data_types/task_container/src/test_aws.py
+++
@@ -1,4 +0,0 @@
-from aws import save_file_to_s3
-
-
-save_file_to_s3(file_path='shopware-ml-ops-interim-prod/projects/avocado_price_prediction/analytical_data_types.json', obj={})
--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/metadata/task_container/docker_build.sh
+++
@@ -1,9 +0,0 @@
-export IMAGE_NAME=xxx
-export IMAGE_TAG=xxx
-export AWS_ACCOUNT_ID=xxx
-export AWS_ACCOUNT_REGION=xxx
-
-aws ecr get-login-password --region $AWS_ACCOUNT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
-docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
