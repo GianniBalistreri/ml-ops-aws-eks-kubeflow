2c9e6f5d04eeb3e87db109202b1fb638d2f333e5
giannibalistreri <gbalistreri762@gmail.com>
2024-02-18 18:00:53 +0100

    Replace sudo command with docker buildx command

--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/analytical_data_types/task_container/docker_build.sh
+++ ml_ops_templates/kubeflow_pipelines/task_pool/v1/analytical_data_types/task_container/docker_build.sh
@@ -1,9 +1,8 @@
-export IMAGE_NAME=xxx
-export IMAGE_TAG=xxx
-export AWS_ACCOUNT_ID=xxx
-export AWS_ACCOUNT_REGION=xxx
+export IMAGE_NAME=ml-ops-analytical-data-types
+export IMAGE_TAG=v1
+export AWS_ACCOUNT_ID=086726293497
+export AWS_ACCOUNT_REGION=eu-central-1
 
 aws ecr get-login-password --region $AWS_ACCOUNT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
-docker build -t $IMAGE_NAME .
-docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
+docker buildx create --user
+docker buildx build --push --tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG --file Dockerfile .
--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/custom_predictor_canary/task_container/docker_build.sh
+++ ml_ops_templates/kubeflow_pipelines/task_pool/v1/custom_predictor_canary/task_container/docker_build.sh
@@ -4,6 +4,5 @@ export AWS_ACCOUNT_ID=xxx
 export AWS_ACCOUNT_REGION=xxx
 
 aws ecr get-login-password --region $AWS_ACCOUNT_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
-sudo docker build -t $IMAGE_NAME .
-sudo docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
-sudo docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
+docker buildx create --user
+docker buildx build --push --tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG --file Dockerfile .
