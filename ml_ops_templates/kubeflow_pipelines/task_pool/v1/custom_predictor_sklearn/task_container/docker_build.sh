a00c99b4f0b97b33566ff05cc5ceacee758b4c4c
giannibalistreri <gbalistreri762@gmail.com>
2024-02-18 18:21:37 +0100

    Refactoring

--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/custom_predictor_sklearn/task_container/docker_build.sh
+++ ml_ops_templates/kubeflow_pipelines/task_pool/v1/custom_predictor_sklearn/task_container/docker_build.sh
@@ -3,7 +3,6 @@ export IMAGE_TAG=xxx
 export AWS_ACCOUNT_ID=xxx
 export AWS_ACCOUNT_REGION=xxx
 
-aws ecr get-login-password --region $AWS_ACCOUNT_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
-sudo docker build -t $IMAGE_NAME .
-sudo docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
-sudo docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
+aws ecr get-login-password --region $AWS_ACCOUNT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
+docker buildx create --user
+docker buildx build --push --tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG --file Dockerfile .
--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/natural_language_processing/task_container/docker_build.sh
+++ ml_ops_templates/kubeflow_pipelines/task_pool/v1/natural_language_processing/task_container/docker_build.sh
@@ -3,6 +3,6 @@ export IMAGE_TAG=xxx
 export AWS_ACCOUNT_ID=xxx
 export AWS_ACCOUNT_REGION=xxx
 
-aws ecr get-login-password --region $AWS_ACCOUNT_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
+aws ecr get-login-password --region $AWS_ACCOUNT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
 docker buildx create --user
 docker buildx build --push --tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG --file Dockerfile .
