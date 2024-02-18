0e49ca668f8a8215b37b421600b0d98e6b977da4
giannibalistreri <gbalistreri762@gmail.com>
2024-02-18 18:03:36 +0100

    Refactoring

--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/analytical_data_types/task_container/docker_build.sh
+++ ml_ops_templates/kubeflow_pipelines/task_pool/v1/analytical_data_types/task_container/docker_build.sh
@@ -1,7 +1,7 @@
-export IMAGE_NAME=ml-ops-analytical-data-types
-export IMAGE_TAG=v1
-export AWS_ACCOUNT_ID=086726293497
-export AWS_ACCOUNT_REGION=eu-central-1
+export IMAGE_NAME=xxx
+export IMAGE_TAG=xxx
+export AWS_ACCOUNT_ID=xxx
+export AWS_ACCOUNT_REGION=xxx
 
 aws ecr get-login-password --region $AWS_ACCOUNT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_ACCOUNT_REGION.amazonaws.com
 docker buildx create --user
