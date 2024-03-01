2d955fbad547346902777e0dab97b598d1391e5e
giannibalistreri <g.balistreri@shopware.com>
2024-03-01 13:19:50 +0100

    Add writing output files

--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/slack_alerting/task_container/src/task.py
+++ ml_ops_templates/kubeflow_pipelines/task_pool/v1/slack_alerting/task_container/src/task.py
@@ -10,6 +10,7 @@ import json
 import urllib3
 
 from custom_logger import Log
+from file_handler import file_handler
 from kubeflow_exit_handler import KubeflowExitHandler
 from typing import NamedTuple
 
@@ -21,6 +22,9 @@ PARSER.add_argument('-header', type=str, required=False, default=None, help='pre
 PARSER.add_argument('-msg', type=str, required=False, default=None, help='pre-defined message')
 PARSER.add_argument('-status', type=int, required=False, default=None, help='pipeline status code')
 PARSER.add_argument('-pipeline_metadata_file_path', type=str, required=False, default=None, help='complete file path of the pipeline metadata')
+PARSER.add_argument('-output_file_path_header', type=str, required=False, default=None, help='file path of the header output')
+PARSER.add_argument('-output_file_path_message', type=str, required=False, default=None, help='file path of the message output')
+PARSER.add_argument('-output_file_path_response_status_code', type=str, required=False, default=None, help='file path of the response status code output')
 ARGS = PARSER.parse_args()
 
 
@@ -34,10 +38,13 @@ class SlackAlertingException(Exception):
 def slack_alerting(exit_handler: bool,
                    aws_region: str,
                    secret_name: str,
+                   output_file_path_header: str,
+                   output_file_path_message: str,
+                   output_file_path_response_status_code: str,
                    header: str = None,
                    msg: str = None,
                    status: int = None,
-                   pipeline_metadata_file_path: str = None
+                   pipeline_metadata_file_path: str = None,
                    ) -> NamedTuple('outputs', [('header', str),
                                                ('message', str),
                                                ('response_status_code', int)
@@ -54,6 +61,15 @@ def slack_alerting(exit_handler: bool,
 
     :param secret_name: str
         Secret name of the secret manager entry containing Slack channel
+
+    :param output_file_path_header: str
+        File path of the header output
+
+    :param output_file_path_message: str
+        File path of the message output
+
+    :param output_file_path_response_status_code: str
+        File path of the response status code output
 
     :param header: str
         Pre-defined header text
@@ -137,6 +153,9 @@ def slack_alerting(exit_handler: bool,
     _encoded_msg: bytes = json.dumps(_slack_msg).encode('utf-8')
     _response = _http.request(method='POST', url=_slack_url, body=_encoded_msg)
     Log().log(msg=f'Slack message send to channel "{_slack_channel_name}" with response status "{_response.status}"')
+    file_handler(file_path=output_file_path_header, obj=_header)
+    file_handler(file_path=output_file_path_message, obj=_message)
+    file_handler(file_path=output_file_path_response_status_code, obj=_response.status)
     return [_header, _message, _response.status]
 
 
@@ -144,6 +163,9 @@ if __name__ == '__main__':
     slack_alerting(exit_handler=bool(ARGS.exit_handler),
                    aws_region=ARGS.aws_region,
                    secret_name=ARGS.secret_name,
+                   output_file_path_header=ARGS.output_file_path_header,
+                   output_file_path_message=ARGS.output_file_path_message,
+                   output_file_path_response_status_code=ARGS.output_file_path_response_status_code,
                    header=ARGS.header,
                    msg=ARGS.msg,
                    status=ARGS.status,
