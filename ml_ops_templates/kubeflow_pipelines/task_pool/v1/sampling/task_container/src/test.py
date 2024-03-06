e3dbf78af581efa0962f94271ccb1e7c23e589bd
giannibalistreri <g.balistreri@shopware.com>
2024-03-06 23:23:29 +0100

    Deletion

--- ml_ops_templates/kubeflow_pipelines/task_pool/v1/sampling/task_container/src/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import unittest
-
-from sampler import MLSampler, Sampler
-
-DATA_SET: pd.DataFrame = pd.read_csv(filepath_or_buffer='https://raw.githubusercontent.com/GianniBalistreri/happy_learning/master/test/data/avocado.csv')
-print(DATA_SET['type'].value_counts(normalize=True))
-
