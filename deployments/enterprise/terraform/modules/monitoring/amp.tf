73f05cf899d2a3fadca8c4570f26d77d119667e7
giannibalistreri <g.balistreri@shopware.com>
2024-03-18 01:08:20 +0100

    Refactoring

--- deployments/enterprise/terraform/modules/monitoring/amp.tf
+++ deployments/enterprise/terraform/modules/monitoring/amp.tf
@@ -4,26 +4,24 @@ resource "aws_prometheus_workspace" "kubeflow" {
   logging_configuration {
     log_group_arn = "${aws_cloudwatch_log_group.prometheus.arn}:*"
   }
-
-  depends_on = [null_resource.create_amp_service_account]
 }
 
-resource "null_resource" "prometheus" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+#resource "null_resource" "prometheus" {
+#  triggers = {
+#    always_run = "${timestamp()}"
+#  }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      export AMP_WORKSPACE_REGION=$(echo ${aws_prometheus_workspace.kubeflow.arn} | cut -d':' -f4)
-      export AMP_WORKSPACE_ID=$(echo ${aws_prometheus_workspace.kubeflow.arn} | cut -d':' -f6 | cut -d'/' -f2)
-      printf '
-      workspaceRegion='$AMP_WORKSPACE_REGION'
-      workspaceId='$AMP_WORKSPACE_ID'
-      ' > ../../../../deployments/add-ons/prometheus/params.env
-      kustomize build ../../../../deployments/add-ons/prometheus | kubectl apply -f -
-    EOT
-  }
+#  provisioner "local-exec" {
+#    command = <<EOT
+#      export AMP_WORKSPACE_REGION=$(echo ${aws_prometheus_workspace.kubeflow.arn} | cut -d':' -f4)
+#      export AMP_WORKSPACE_ID=$(echo ${aws_prometheus_workspace.kubeflow.arn} | cut -d':' -f6 | cut -d'/' -f2)
+#      printf '
+#      workspaceRegion='$AMP_WORKSPACE_REGION'
+#      workspaceId='$AMP_WORKSPACE_ID'
+#      ' > ../../../../deployments/add-ons/prometheus/params.env
+#      kustomize build ../../../../deployments/add-ons/prometheus | kubectl apply -f -
+#    EOT
+#  }
 
-  depends_on = [aws_prometheus_workspace.kubeflow]
-}
+#  depends_on = [aws_prometheus_workspace.kubeflow]
+#}
